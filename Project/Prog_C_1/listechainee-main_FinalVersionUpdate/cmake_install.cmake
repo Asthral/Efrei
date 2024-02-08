68cefe9942e3904610acf5521e2927ded37f1b06
Asthral <151788916+Asthral@users.noreply.github.com>
2024-02-08 16:27:10 +0100

    Update cmake_install.cmake

--- Project/Prog_C_1/listechainee-main_FinalVersionUpdate/cmake_install.cmake
+++ Project/Prog_C_1/listechainee-main_FinalVersionUpdate/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ssor/cdev/listechainee
+# Install script for directory: /home/Asthral/cdev/listechainee
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ssor/cdev/listechainee/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/Asthral/cdev/listechainee/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
